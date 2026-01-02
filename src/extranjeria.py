47f15a2eb430e40d82844bce7b6c8455b8c4e254
Francisco-Javier-Jimenez-Garcia <146074993+Francisco-Javier-Jimenez-Garcia@users.noreply.github.com>
2026-01-02 20:41:24 +0100

    Commit prueba

---
+++ src/extranjeria.py
@@ -0,0 +1,16 @@
+from collections import NamedTuple
+
+RegistroExtranjeria = NamedTuple(
+    "RegistroExtranjeria", 
+            [("distrito",str),
+             ("seccion", str),
+             ("barrio", str),
+             ("pais",str),
+             ("hombres", int),
+             ("mujeres", int)
+            ]
+)
+
+
+def lee_datos_extranjeria(fichero):
+    
